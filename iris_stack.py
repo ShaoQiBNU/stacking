edf2b7e96809f9268f40ef7bcc58e852c21522fe
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2018-09-21 11:40:03 +0800

    Update iris_stack.py

--- iris_stack.py
+++ iris_stack.py
@@ -39,3 +39,25 @@ for clf, label in zip([clf1, clf2, clf3, sclf],
     
     print("Accuracy: %0.2f (+/- %0.2f) [%s]" 
           % (scores.mean(), scores.std(), label))
+    
+    
+import matplotlib.pyplot as plt
+from mlxtend.plotting import plot_decision_regions
+import matplotlib.gridspec as gridspec
+import itertools
+
+gs = gridspec.GridSpec(2, 2)
+
+fig = plt.figure(figsize=(10,8))
+
+for clf, lab, grd in zip([clf1, clf2, clf3, sclf], 
+                         ['KNN', 
+                          'Random Forest', 
+                          'Naive Bayes',
+                          'StackingClassifier'],
+                          itertools.product([0, 1], repeat=2)):
+
+    clf.fit(X, y)
+    ax = plt.subplot(gs[grd[0], grd[1]])
+    fig = plot_decision_regions(X=X, y=y, clf=clf)
+    plt.title(lab)
